5d4d12e4c604f3e87145af64cda9087fe8b701df
nachoaladro <ignacioaladro385@gmail.com>
2023-12-02 21:58:47 -0300

    Corregi el background-color para ajustarlo al logo

--- src/components/NavBarComponent/NavBarComponent.jsx
+++ src/components/NavBarComponent/NavBarComponent.jsx
@@ -2,22 +2,42 @@ import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 
+import "./NavBarComponent.css";
+
 
 const NavBarComponent = () => {
 
     return (
-        
-        <Navbar bg="light" data-bs-theme="light">
-            <Container>
-                <Navbar.Brand href="#home">Navbar</Navbar.Brand>
-                <Nav className="me-auto">
-                    <Nav.Link href="#home">Home</Nav.Link>
-                    <Nav.Link href="#features">Features</Nav.Link>
-                    <Nav.Link href="#pricing">Pricing</Nav.Link>
-                </Nav>
-            </Container>
-        </Navbar>
 
+        <div>
+
+            <div className="container">
+                <p>
+                    <a href="tel:+542324504614" className="sinDeco">+54 9 2324 504614</a> |{' '}
+                    <a href="mailto:clararevora@gmail.com" target="_blank" rel="noopener noreferrer" className="sinDeco">
+                        clararevora@gmail.com
+                    </a>
+                </p>
+            </div>
+        
+            <div>
+                <Navbar data-bs-theme="light">
+                    <Container>
+                        <a href="#home">
+                            <img src="imgs/logo.png" alt="" />
+                        </a>
+                        <Nav className="me-auto">
+                            <Nav.Link href="#new-in">NEW IN</Nav.Link>
+                            <Nav.Link href="#tienda">TIENDA</Nav.Link>
+                            <Nav.Link href="#rebajas">REBAJAS</Nav.Link>
+                            <Nav.Link href="#como-comprar">COMO COMPRAR</Nav.Link>
+                            <Nav.Link href="#contacto">CONTACTO</Nav.Link>
+                        </Nav>
+                    </Container>
+                </Navbar>
+            </div>
+
+        </div>
     );
 
 };
