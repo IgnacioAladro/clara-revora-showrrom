f21bcba68041d6a8fcc66b23229f1c8f1577d2cc
nachoaladro <ignacioaladro385@gmail.com>
2023-12-04 01:03:39 -0300

    Agrege CartWidget + notificacion hardcodeada

--- src/components/NavBarComponent/NavBarComponent.jsx
+++ src/components/NavBarComponent/NavBarComponent.jsx
@@ -24,11 +24,13 @@ const NavBarComponent = () => {
             <div>
                 <Navbar data-bs-theme="light">
                     <Container className='d-flex justify-content-between'>
+
                         <div>
                             <a href="index.html">
                                 <img src={logo} alt="Logo de Clara Revora Showroom" />
                             </a>
                         </div>
+
                         <div>
                             <Nav className="me-auto gap-5 me-navBar-style">
                                 <Nav.Link href="#new-in">NEW IN</Nav.Link>
@@ -38,6 +40,14 @@ const NavBarComponent = () => {
                                 <Nav.Link href="#contacto">CONTACTO</Nav.Link>
                             </Nav>
                         </div>
+
+                        <div>
+                            <a href="#cart-shop" className='sinDeco'>
+                                <svg xmlns="http://www.w3.org/2000/svg" width="35" height="35" viewBox="0 0 24 24"><path fill="currentColor" d="M19 7h-3V6a4 4 0 0 0-8 0v1H5a1 1 0 0 0-1 1v11a3 3 0 0 0 3 3h10a3 3 0 0 0 3-3V8a1 1 0 0 0-1-1Zm-9-1a2 2 0 0 1 4 0v1h-4Zm8 13a1 1 0 0 1-1 1H7a1 1 0 0 1-1-1V9h2v1a1 1 0 0 0 2 0V9h4v1a1 1 0 0 0 2 0V9h2Z"/></svg>
+                                4
+                            </a>
+                        </div>
+                            
                     </Container>
                 </Navbar>
             </div>
